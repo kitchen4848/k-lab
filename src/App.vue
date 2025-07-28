6951b583841f6e56164229c1b7e32bab23a07955
k-neko <kitchen00600@gmail.com>
2025-07-28 22:20:44 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h1 class="text-3xl font-bold underline text-blue-600">Hello Tailwind!</h1>
+  </header>
+
+</template>
+
+<style scoped></style>
